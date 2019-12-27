368089ea94f9361ea1da17eac971b18ce2f1ca7c
Thiago Henrique Santos <thiago.santos@fcamara.com.br>
2019-12-27 12:02:38 -0300

    [ADD] Migration trainer.

---
+++ src/database/migrations/20191227144123-create-trainer.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('trainers', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => { queryInterface.dropTable('trainers'); },
+};
