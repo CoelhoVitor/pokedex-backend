b6529f31d5c7619721e3ace8c2467764215ef0e4
Thiago Henrique Santos <thiago.santos@fcamara.com.br>
2019-12-27 13:35:15 -0300

    [ADD] Configuração do banco de dados.

--- src/database/migrations/20191227144123-create-trainer.js
+++ src/database/migrations/20191227144123-create-trainer.js
@@ -19,6 +19,14 @@ module.exports = {
       type: Sequelize.STRING,
       allowNull: false,
     },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
   }),
 
   down: (queryInterface) => { queryInterface.dropTable('trainers'); },
