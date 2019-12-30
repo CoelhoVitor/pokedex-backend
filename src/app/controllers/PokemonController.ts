ad0e9121a3d9374d002624c752d48939cef513f4
CoelhoVitor <vitorvcs@hotmail.com>
2019-12-30 13:29:00 -0300

    [ADD] Pokemon validations

--- src/app/controllers/PokemonController.ts
+++ src/app/controllers/PokemonController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import Pokemon from '../models/Pokemon';
 
@@ -10,13 +11,41 @@ class PokemonController {
   }
 
   public async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      type1: Yup.string().required(),
+      type2: Yup.string(),
+      weakness: Yup.string(),
+      resistence: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const pokemon = await Pokemon.create(req.body);
 
     return res.json(pokemon);
   }
 
   public async update(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      type1: Yup.string(),
+      type2: Yup.string(),
+      weakness: Yup.string(),
+      resistence: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
     const pokemon = await Pokemon.findByPk(req.params.id);
+
+    if (!pokemon) {
+      return res.status(400).json({ error: 'Pokémon inexistente' });
+    }
 
     const pokemonUpdated = await pokemon.update(req.body);
 
@@ -26,9 +55,13 @@ class PokemonController {
   public async delete(req: Request, res: Response): Promise<Response> {
     const pokemon = await Pokemon.findByPk(req.params.id);
 
+    if (!pokemon) {
+      return res.status(400).json({ error: 'Pokémon inexistente' });
+    }
+
     await pokemon.destroy();
 
-    return res.send();
+    return res.json({ message: 'Pokémon excluído com sucesso' });
   }
 }
 
